29622858f0d6d25e08d31148c4ec5c2053f00bf0
absnormal <37442154+absnormal@users.noreply.github.com>
2023-10-19 22:31:22 +0800

    change url to static

--- src/components/CustomizableCharacterPool.vue
+++ src/components/CustomizableCharacterPool.vue
@@ -5,9 +5,9 @@ import { addLegionNumber } from './functions/addLegionNumber.js'
 import list from './json/charData.json'
 import OptimizeLinks from './OptimizeLinks.vue'
 
-const path = "./src/components/imgs/"
-const classPath = "./src/components/imgs/occupation/"
-const cardPath = "./src/components/imgs/card/Card_"
+const path = "@/src/components/imgs/"
+const classPath = "@/src/components/imgs/occupation/"
+const cardPath = "@/src/components/imgs/card/Card_"
 const props = defineProps({
 	page: Object
 });
